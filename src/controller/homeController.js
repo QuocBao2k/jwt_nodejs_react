2afee7f7054b964bfee71d98d86e3c3d9feb3d29
Bao Huynh <quocbao2kdev@gmail.com>
2023-09-20 15:05:38 +0700

    video5 crud with nodejs and link with database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,40 @@
+// get the client
+const mysql = require("mysql2");
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "jwt",
+});
+
+//
 const handleHome = (req, res) => {
   return res.render("home.ejs");
 };
+const handleUserPage = (req, res) => {
+  return res.render("user.ejs");
+};
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+  // simple query
 
+  connection.query(
+    `INSERT INTO users(email,password,username) VALUES (?,?,?)`,
+    [email, password, username],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  //
+  return res.send("handleCreateNewUser");
+};
 module.exports = {
   handleHome,
+  handleUserPage,
+  handleCreateNewUser,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { handleHome } from "../controller/homeController";
+import homeController from "../controller/homeController";
 const router = express.Router();
 /**
  *
  * @param {*} app : exoress app
  */
 const initWebRoutes = (app) => {
-  router.get("/", handleHome);
+  router.get("/", homeController.handleHome);
+  router.get("/user", homeController.handleUserPage);
+  router.post("/users/create-user", homeController.handleCreateNewUser);
+
   return app.use("/", router);
 };
 export default initWebRoutes;
