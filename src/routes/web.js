640a3dbef074fd023d4e653763ff27076455d0ae
Bao Huynh <quocbao2kdev@gmail.com>
2023-09-20 09:59:25 +0700

    video 4

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,14 +1,12 @@
 import express from "express";
-
+import { handleHome } from "../controller/homeController";
 const router = express.Router();
 /**
  *
  * @param {*} app : exoress app
  */
 const initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("hello world");
-  });
+  router.get("/", handleHome);
   return app.use("/", router);
 };
 export default initWebRoutes;
